8ada24a10853db721174d910934bef2487e82286
Michael Pu <michael.pu123@gmail.com>
2018-12-22 09:59:09 -0500

    Added API endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const config = require('./config.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	response.send("Hello from Firebase!");
+const {dialogflow} = require('actions-on-google');
+
+const app = dialogflow({
+    debug: true
+});
+
+app.intent('Retrieve-Next-Arrival-Time', (conv) => {
+    conv.ask('Getting.');
+});
+
+exports.dialogflowHandler = functions.https.onRequest(app);
+
+exports.api = functions.https.onRequest((req, res) => {
+    if (req.method === 'POST') {
+        const body = req.body;
+        const headers = req.headers;
+        if (headers.key === config.key) {
+            res.send('Authorized');
+        } else {
+	        res.status(401).send("Unauthorized.");
+        }
+    }
+	res.send("Bus-Predictor API.");
 });
