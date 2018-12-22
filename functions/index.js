c7c60d0796ebc3ab5f4934321824227d334fd7ce
Michael Pu <michael.pu123@gmail.com>
2018-12-22 14:37:13 -0500

    Added welcome intent

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const config = require('./config.json');
 
-const {dialogflow} = require('actions-on-google');
+const {dialogflow, Permission} = require('actions-on-google');
 
 const app = dialogflow({
     debug: true
@@ -9,6 +9,26 @@ const app = dialogflow({
 
 app.intent('Retrieve-Next-Arrival-Time', (conv) => {
     conv.ask('Getting.');
+});
+
+app.intent('Welcome-Intent', (conv) => {
+    const options = {
+        context: "In order to proceed, I need you location.",
+        permissions: ['DEVICE_PRECISE_LOCATION']
+    };
+    conv.ask(new Permission(options));
+});
+
+app.intent('Process-Location', (conv, params, confirmationGranted) => {
+    if (confirmationGranted) {
+        if (conv.data === 'DEVICE_PRECISE_LOCATION') {
+            conv.ask('You are at ' + conv.device.location.coordiantes);
+        } else {
+            conv.ask('Wrong info sent.');
+        }
+    } else {
+        conv.ask('Sorry, permission denied.');
+    }
 });
 
 exports.dialogflowHandler = functions.https.onRequest(app);
