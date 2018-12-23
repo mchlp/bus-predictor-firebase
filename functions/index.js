574f391e6baf7bdbde14456fe593d9128b4d9169
Michael Pu <michael.pu123@gmail.com>
2018-12-23 16:09:59 -0500

    Added bus predictions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const googlePolyline = require('google-polyline');
 const axios = require('axios');
 const config = require('./config.json');
 const xmlParser = require('xml-parser');
@@ -8,26 +9,128 @@ const globals = {
     apiKey: config['cloud-platform']['api-key'],
     nextBusUrl: config['nextbus-api']['root-url'],
     agency: config['nextbus-api']['agency'],
-}
+    mapsUrl: config['google-maps-api']['root-url']
+};
 
 const app = dialogflow({
     debug: false
 });
 
-app.intent('Retrieve-Next-Arrival-Time', async (conv) => {
-    const routeNum = conv.parameters['route-number'];
-    const res = await axios.get(globals.nextBusUrl + '?command=routeConfig&a=' + globals.agency + '&r=' + routeNum);
-    const parsedData = xmlParser(res.data);
-    const routeInfo = parsedData['root']['children'][0];
-    const stops = [];
-    for (let i=0; i<routeInfo.children.length; i++) {
-        const childEle = routeInfo.children[i];
-        if (childEle.name === 'stop') {
-            stops.push(childEle);
-        }
+const parseMinutes = (minutes) => {
+    if (minutes === 1) {
+        return '1 minute';
+    } else {
+        return minutes + ' minutes';
     }
-    console.log(stops);
-    conv.ask('Getting for route ' + routeNum);
+};
+
+const getDistance = (x1, y1, x2, y2) => {
+    return Math.sqrt(Math.pow(Math.abs(Number.parseFloat(x1) - Number.parseFloat(x2)), 2) + Math.pow(Math.abs(Number.parseFloat(y1) - Number.parseFloat(y2)), 2));
+};
+
+const sortStopsByDistance = (a, b) => {
+    if (a.distance > b.distance) {
+        return 1;
+    } else if (a.distance === b.distance) {
+        return 0;
+    } else {
+        return -1;
+    }
+};
+
+app.intent('Retrieve-Next-Arrival-Time', async (conv) => {
+    const location = conv.user.storage.location;
+    const routeBranch = conv.parameters['route-branch'];
+    const routeNum = routeBranch['branch-number'];
+    const routeLetter = routeBranch['branch-letter'];
+    const routeDirection = conv.parameters['route-direction'];
+    const routeInfoRes = await axios.get(globals.nextBusUrl + '?command=routeConfig&a=' + globals.agency + '&r=' + routeNum);
+    const parsedRouteInfoData = xmlParser(routeInfoRes.data);
+    const routeInfo = parsedRouteInfoData['root']['children'][0];
+    if (routeInfo.children.length > 0) {
+        const allStops = {};
+        for (let i = 0; i < routeInfo.children.length; i++) {
+            const stopEle = routeInfo.children[i];
+            if (stopEle.name === 'stop') {
+                let normalizedStopEle = stopEle.attributes;
+                normalizedStopEle.distance = getDistance(location[0], location[1], normalizedStopEle.lat, normalizedStopEle.lon);
+                allStops[normalizedStopEle.tag] = normalizedStopEle;
+            }
+        }
+
+        let filteredStopsList = [];
+        for (let i = 0; i < routeInfo.children.length; i++) {
+            const directionEle = routeInfo.children[i];
+            if (directionEle.name === 'direction') {
+                if (directionEle.attributes.name.toUpperCase() === routeDirection.toUpperCase() && (!routeLetter || directionEle.attributes.branch.toUpperCase() === (routeNum + routeLetter.toUpperCase()))) {
+                    for (let j = 0; j < directionEle.children.length; j++) {
+                        const stopEle = directionEle.children[j];
+                        if (stopEle.name === 'stop') {
+                            filteredStopsList.push(allStops[stopEle.attributes.tag]);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        if (filteredStopsList.length > 0) {
+            filteredStopsList.sort(sortStopsByDistance);
+            const closestStops = filteredStopsList.slice(0, config['google-maps-api']['distance-matrix-batch-size']);
+            const stopCoordinates = [];
+            for (let i = 0; i < closestStops.length; i++) {
+                stopCoordinates.push([closestStops[i].lat, closestStops[i].lon]);
+            }
+            const encodedLocation = googlePolyline.encode([location]);
+            const encodedStops = googlePolyline.encode(stopCoordinates);
+            const distanceMatrixRes = await axios.get(globals.mapsUrl + '/distancematrix/json?units=metric&origins=enc:' + encodedLocation + ':&destinations=enc:' + encodedStops + ':&mode=walking&key=' + globals.apiKey);
+            const destDistanceMatrix = distanceMatrixRes.data.rows[0].elements;
+            let minDistance = Number.MAX_VALUE;
+            let minStop;
+            for (let i = 0; i < destDistanceMatrix.length; i++) {
+                if (destDistanceMatrix[i].status === 'OK') {
+                    if (minDistance > destDistanceMatrix[i].distance.value) {
+                        minDistance = destDistanceMatrix[i].distance.value;
+                        minStop = closestStops[i];
+                    }
+                }
+            }
+
+            let predictionMessage = 'Your closest stop for this route is ' + minStop.title + '.';
+
+            const predictionRes = await axios.get(globals.nextBusUrl + '?command=predictions&a=' + globals.agency + '&r=' + routeNum + '&s=' + minStop.tag);
+            const parsedPredictionData = xmlParser(predictionRes.data);
+            const predictionInfo = parsedPredictionData['root']['children'][0];
+
+            if (predictionInfo.children.length === 1) {
+                const predictionsList = predictionInfo.children[0];
+                predictionMessage += ' The next buses for the route ' + predictionsList.attributes.title + ' are expected to arrive in ';
+                for (let i = 0; i < predictionsList.children.length; i++) {
+                    const prediction = predictionsList.children[i];
+                    predictionMessage += parseMinutes(prediction.attributes.minutes);
+                    if (i === predictionsList.children.length - 2) {
+                        predictionMessage += ', and ';
+                    } else if (i === predictionsList.children.length - 1) {
+                        predictionMessage += '.';
+                    } else {
+                        predictionMessage += ', ';
+                    }
+                }
+            } else if (predictionInfo.children.length > 1) {
+                predictionMessage = ' No predictions are currently available for this stop.';
+            } else {
+                predictionMessage = ' No predictions are currently available for this stop.';
+            }
+
+            console.log(predictionInfo);
+
+            conv.ask(predictionMessage);
+        } else {
+            conv.ask('The route you specified could not be found.');
+        }
+    } else {
+        conv.ask('The route you specified could not be found.');
+    }
 });
 
 app.intent('Welcome-Intent', (conv) => {
@@ -41,9 +144,9 @@ app.intent('Welcome-Intent', (conv) => {
 app.intent('Process-Location', async (conv, params, confirmationGranted) => {
     if (confirmationGranted) {
         const coordinates = conv.device.location.coordinates;
-        const res = await axios.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + coordinates.latitude + ',' + coordinates.longitude + '&location_type=ROOFTOP&result_type=street_address&key=' + globals.apiKey);
+        const res = await axios.get(globals.mapsUrl + '/geocode/json?latlng=' + coordinates.latitude + ',' + coordinates.longitude + '&location_type=ROOFTOP&result_type=street_address&key=' + globals.apiKey);
         resData = res.data;
-        if (resData['results'].length > 0) {
+        if (resData['status'] === 'OK') {
             conv.ask('Your location was detected as ' + resData['results'][0]['formatted_address'] + '. Is this correct or close to your current location?');
         } else {
             conv.ask('Your location could not be determined.');
@@ -54,7 +157,7 @@ app.intent('Process-Location', async (conv, params, confirmationGranted) => {
 });
 
 app.intent('Process-Location-Yes', (conv) => {
-    conv.user.storage.location = conv.device.location.coordinates;
+    conv.user.storage.location = [conv.device.location.coordinates.latitude, conv.device.location.coordinates.longitude];
     conv.ask('Alright. Your location has been saved.');
 });
 
